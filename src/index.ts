32d9fb25f79f612773faff01beea5c3eee8460b0
GabrielSilper <gabrielsilper.main@gmail.com>
2024-01-14 19:25:01 -0400

    email sendo enviado com sucesso!

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-async function main() {
-  console.log('Hello World');
+import { transporter, user } from './transporter';
+
+async function sendEmail() {
+  await transporter.sendMail({
+    from: `Gabriel Pereira <${user}>`,
+    to: 'gabrielsilper93@gmail.com',
+    subject: 'Teste de email com nodemailer',
+    html: '<h1>Teste de email com nodemailer</h1>',
+  });
+  console.log('Email enviado!');
 }
 
-main();
+sendEmail();
--- src/transporter.ts
+++ src/transporter.ts
@@ -1,6 +1,14 @@
 import nodemailer from 'nodemailer';
 
-// export const user = process.env.MAIL_USER;
-// const pass = process.env.MAIL_PASS;
+export const user = process.env.MAIL_USER;
+const pass = process.env.MAIL_PASS;
 
-
+export const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user,
+    pass,
+  },
+});
